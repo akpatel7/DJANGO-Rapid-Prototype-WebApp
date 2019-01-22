b857f9c0ba4d6e3396c3e67f501d434a41b99bb3
Amit Patel <patel_amitk@hotmail.com>
2019-01-21 22:29:42 -0500

    added tests, permissions, users, form, and validations

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qq@6hglqb8s8ms1(hth=84i_e)4gr7+($nbs=l1xh+j!0=tw1g'
+#SECRET_KEY = 'qq@6hglqb8s8ms1(hth=84i_e)4gr7+($nbs=l1xh+j!0=tw1g'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
